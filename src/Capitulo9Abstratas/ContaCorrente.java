d2847a7236157a3db66331e0fd3c182c0a0e8ce5
Olavio Lacerda <olavio.lacerda@hotmail.com>
2016-04-10 20:48:43 -0300

    Added files via upload

---
+++ src/Capitulo9Abstratas/ContaCorrente.java
@@ -0,0 +1,13 @@
+package Capitulo9Abstratas;
+
+public class ContaCorrente extends Conta {
+	@Override
+	public void atualiza(double taxa) {
+		this.saldo += this.saldo * taxa * 2;
+	}
+
+	@Override
+	public void deposita(double valor) {
+		this.saldo += valor - 0.10;
+	}
+}
