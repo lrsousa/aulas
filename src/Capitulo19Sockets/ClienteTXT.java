6f44935039413adafa2dce4673ede15662b363af
olaviolacerda <olavio.lacerda@hotmail.com>
2016-05-18 20:24:31 -0300

    Jaba 8 para escrever arquivo

--- src/Capitulo19Sockets/ClienteTXT.java
+++ src/Capitulo19Sockets/ClienteTXT.java
@@ -11,7 +11,7 @@ import org.omg.CORBA.portable.UnknownException;
 
 public class ClienteTXT {
 	public static void main(String[] args) throws UnknownException, IOException {
-		Socket cliente = new Socket("127.0.0.1", 12346);
+		Socket cliente = new Socket("127.0.0.1", 4444);
 		System.out.println("O cliente se conectou ao servidor!");
 
 		InputStream is = new FileInputStream("arquivo.txt");
--- src/Capitulo19Sockets/Servidor.java
+++ src/Capitulo19Sockets/Servidor.java
@@ -1,21 +1,31 @@
 package Capitulo19Sockets;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Servidor {
 	public static void main(String[] args) throws IOException {
-		ServerSocket servidor = new ServerSocket(12345);
-		System.out.println("Porta 1234 aberta!");
+		ServerSocket servidor = new ServerSocket(4444);
+		System.out.println("Porta 44441 aberta!");
 
 		Socket cliente = servidor.accept();
 		System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
 		Scanner entrada = new Scanner(cliente.getInputStream());
+		
 		while (entrada.hasNextLine()) {
-			System.out.println(entrada.nextLine());
+			try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("saida.txt"))) {
+				writer.append(entrada.nextLine());
+				writer.append("\n");
+			}
+//			System.out.println(entrada.nextLine());
 		}
 
 		entrada.close();
