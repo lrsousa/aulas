a51f90d5c5e9d2998e2dcb2bc26c92a05acc3bf8
olaviolacerda <olavio.lacerda@hotmail.com>
2016-05-18 20:50:29 -0300

    mais uma tentativa, ajuda do professor

--- src/Capitulo19Sockets/Servidor.java
+++ src/Capitulo19Sockets/Servidor.java
@@ -1,33 +1,39 @@
 package Capitulo19Sockets;
 
+import java.nio.file.*;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Servidor {
 	public static void main(String[] args) throws IOException {
+		BufferedWriter writer =  null;
 		ServerSocket servidor = new ServerSocket(4444);
-		System.out.println("Porta 44441 aberta!");
+		System.out.println("Porta 4444 aberta!");
 
 		Socket cliente = servidor.accept();
 		System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
 		Scanner entrada = new Scanner(cliente.getInputStream());
-		
-		while (entrada.hasNextLine()) {
-			try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("saida.txt"))) {
-				writer.append(entrada.nextLine());
-				writer.append("\n");
-			}
-//			System.out.println(entrada.nextLine());
-		}
+		String linha = "";
 
+		try { 
+			writer = Files.newBufferedWriter(Paths.get("saida.txt"));
+		} catch (IOException e) {
+			entrada.close();
+			servidor.close();
+			throw new RuntimeException(e);
+		}
+		
+	while (entrada.hasNextLine()) {
+			linha = entrada.nextLine();
+			writer.append(linha + "\n");
+	}		
+		
 		entrada.close();
 		servidor.close();
 	}
